ffe623f529792a8a24ea70fe1077fe948e972788
KurtReti <kurt.reti@gmail.com>
2022-09-05 00:25:02 +1000

    added basic js function and fixed header images

---
+++ script.js
@@ -0,0 +1,11 @@
+function dailyPeriod(){
+    document.getElementById("daily").style.color = "white";
+};
+
+function weeklyPeriod(){
+    document.getElementById("weekly").style.color = "white";
+};
+
+function monthlyPeriod(){
+    document.getElementById("monthly").style.color = "white";
+};
